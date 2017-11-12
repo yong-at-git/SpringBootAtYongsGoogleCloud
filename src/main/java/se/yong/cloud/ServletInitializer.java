ad937397a160a96a64629d7297c62d2f48d5fa02
Yong Huang <huangyong0906@gmail.com>
2017-11-12 18:36:31 +0100

    Adapt Spring Boot to run in Google Cloud platform AppEngine. Refer: https://github.com/GoogleCloudPlatform/getting-started-java/tree/master/appengine-standard-java8/springboot-appengine-standard

---
+++ src/main/java/se/yong/cloud/ServletInitializer.java
@@ -0,0 +1,16 @@
+package se.yong.cloud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Created by Yong Huang <yong.huang@mitel.com> on 2017-11-12.
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+
+}
