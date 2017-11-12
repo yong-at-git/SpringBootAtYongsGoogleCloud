58f6fc52f6e459e58cf7a20e3fa73589e5389863
Yong Huang <huangyong0906@gmail.com>
2017-11-12 19:28:58 +0100

    Add reminder msg while visiting from root url.

--- src/main/java/se/yong/cloud/App.java
+++ src/main/java/se/yong/cloud/App.java
@@ -1,15 +1,38 @@
 package se.yong.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Yong Huang <yong.e.huang@gmail.com> on 2017-11-12.
  */
 @SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
+    }
+
+    @RequestMapping("/")
+    public ResponseEntity<RootHelpResponse> getHelp() {
+        RootHelpResponse response = new RootHelpResponse("See available APIs from /docs");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    class RootHelpResponse {
+        private String message;
+
+        public RootHelpResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
